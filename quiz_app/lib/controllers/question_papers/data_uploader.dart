1aef176f68bb03402539fa183072c35a268bfd31
IdaDanuartha <danuart21@gmail.com>
2023-08-01 14:43:07 +0800

    fill the data_uploader (controller), question_paper_model (model) and data_uploader_screen (view)

---
+++ quiz_app/lib/controllers/question_papers/data_uploader.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:quiz_app/models/question_paper_model.dart';
+
+class DataUploader extends GetxController {
+  @override
+  void onReady() {
+    uploadData();
+    super.onReady();
+  }
+
+  Future<void> uploadData() async {
+    final manifestContent = await DefaultAssetBundle.of(Get.context!)
+      .loadString("AssetManifest.json");
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+
+    // load json file and print path
+    final papersInAssets = manifestMap.keys
+      .where((path) => path.startsWith("assets/DB/paper") && path.contains(".json"))
+      .toList();
+
+      List<QuestionPaperModel> questionPapers = [];
+
+      for(var paper in papersInAssets) {
+        String stringPaperContent = await rootBundle.loadString(paper);
+        questionPapers.add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
+      }
+      print('Items number ${questionPapers[0].id}');
+  }
+}
